4d0714fdb953f9288879b273c2db565bc92a5d24
TopScrech <sergei_saliukov@icloud.com>
2026-01-20 00:42:02 +0100

    Initial Commit

---
+++ Rename Files to UUID/Rename_Files_to_UUIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rename_Files_to_UUIDApp.swift
+//  Rename Files to UUID
+//
+//  Created by Sergei Saliukov on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Rename_Files_to_UUIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
